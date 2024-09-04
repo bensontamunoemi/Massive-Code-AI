06dc46d5d853fffe5fbff5ba350e4774d414d916
Tamunoemi Benson <62636484+bensontamunoemi@users.noreply.github.com>
2024-09-04 11:09:49 -0400

    Update model

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.post('/', async (req, res) => {
     const prompt = req.body.prompt;
 
     const response = await openai.createCompletion({
-      model: 'text-davinci-003',
+      model: 'gpt-3.5-turbo-0125',
       prompt: `${prompt}`,
       temperature: 0,
       max_tokens: 3000,
